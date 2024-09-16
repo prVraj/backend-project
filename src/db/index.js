44fac0c7112891a67d942059350b179aab342489
Vraj Parikh <vrajparikh169@gmail.com>
2024-09-16 10:08:25 +0530

    establised connection

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectTOdb = async () => {
+  try {
+    const connectDB = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n DataBase connected!! DB Host: ${connectDB.connection.host}`
+    );
+  } catch (error) {
+    console.error(`MONGODB connection FAILED: ${error}`);
+    process.exit(1);
+  }
+};
+
+
+export default connectTOdb;
