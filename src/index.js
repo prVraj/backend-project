4b44e6b46a8ec6553c2e45ebf1946f3efd7fba83
Vraj Parikh <vrajparikh169@gmail.com>
2024-09-16 23:38:11 +0530

    adding utilities

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import dotenv from "dotenv" 
-import connectTOdb from "./db/index.js"
+import dotenv from "dotenv";
+import connectTOdb from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-  path: './.env'
+  path: "./.env",
 });
 
 connectTOdb()
+  .then(() => {
+    try {
+      app.on("Error", (err) => {
+        console.error(`Error: ${err}`);
+      });
 
+      app.listen(process.env.PORT || 5000, () => {
+        console.log(`⚙️ Server run at port: ${process.env.PORT}`);
+      });
+    } catch (error) {
+      console.error(`can't listen: ${error}`);
+    }
+  })
+  .catch((error) => {
+    console.error(`MONGODB connection FAILED: ${error}`);
+  });
 
 /*
 const app = express();
---
+++ src/utils/ApiError.js
@@ -0,0 +1,20 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message = "somthing went wrong!!",
+        statct = "",
+        errors = []
+    ) {
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.errors = errors
+
+        if (statct) {
+            this.statct = statct
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
